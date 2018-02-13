55b0f83d7d83158c0292ec1c39d451f3018352a0
Erik the Viking <erik@hhd.com.au>
2018-02-13 12:58:09 +1100

    implement run functions

--- Lox.java
+++ Lox.java
@@ -18,4 +18,26 @@ public class Lox {
       runPrompt();
     }
   }
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) { 
+      System.out.print("> ");
+      run(reader.readLine());
+    }
+  }
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
 }
