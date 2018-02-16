ec9ca0f9c882bc330130a1e9a51bd652d31c4362
Erik the Viking <erik@hhd.com.au>
2018-02-16 13:37:17 +1100

    update upto and add comment

--- Token.java
+++ Token.java
@@ -4,6 +4,7 @@ class Token {
   final TokenType type;
   final String lexeme;
   final Object literal;
+ //Our job is to scan through the list of characters and group them together into the smallest sequences that still represent something. Each of these blobs of characters is called a lexeme.
   final int line; 
 
   Token(TokenType type, String lexeme, Object literal, int line) {
