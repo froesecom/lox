7d6f382ddce955d3610fd0e24d012341a554063c
Erik the Viking <erik@hhd.com.au>
2018-03-12 12:10:12 +1100

    handle comments when scanning

--- Scanner.java
+++ Scanner.java
@@ -46,6 +46,14 @@ class Scanner {
       case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
       case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
       case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+      case '/':
+        if (match('/')) {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+        break;
       default:
         Lox.error(line, "Unexpected character.");
         break;
@@ -58,6 +66,11 @@ class Scanner {
 
     current++;
     return true;
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
   }
 
   private boolean isAtEnd() {
