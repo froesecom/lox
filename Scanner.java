3f43cc18603855d57e50bdaf501cfdcc739ba509
Erik the Viking <erik@hhd.com.au>
2018-04-05 13:17:41 +1000

    scan for number literals

--- Scanner.java
+++ Scanner.java
@@ -65,9 +65,28 @@ class Scanner {
         break;
       case '"': string(); break;
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
   }
 
   private void string() {
@@ -102,6 +121,15 @@ class Scanner {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
   }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  } 
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  } 
 
   private boolean isAtEnd() {
     return current >= source.length();
