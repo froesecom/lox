8b42db1425a9b3ca18f0971032d746d7883f94c0
Erik the Viking <erik@hhd.com.au>
2018-04-26 17:32:14 +1000

    finish section 4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lox
 My implementation of the Lox language, from [Crafting Interpreters](http://www.craftinginterpreters.com)
+
+`javac Lox.java` to compile
+`java Lox` to run
--- Scanner.java
+++ Scanner.java
@@ -13,6 +13,27 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
 
   Scanner(String source) {
       this.source = source;
@@ -67,11 +88,24 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    // See if the identifier is a reserved word.
+    String text = source.substring(start, current);
+
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
   }
 
   private void number() {
@@ -126,6 +160,16 @@ class Scanner {
     if (current + 1 >= source.length()) return '\0';
     return source.charAt(current + 1);
   } 
+  
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
 
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
